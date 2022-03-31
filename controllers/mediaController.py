c6ce75cc1f7dd8e04994356e3343db9d7a1ad280
Adrien Leib <adrienleib@MacBook-Pro-de-Adrien.local>
2022-03-31 17:05:21 +0200

    get films  from netflixMedia

--- controllers/mediaController.py
+++ controllers/mediaController.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 import requests
 
-def get_medias(id):
-    r = requests.get('127.0.0.1:5000/films')
-    return jsonify(r.json), 200
+def get_medias():
+    r = requests.get('http://127.0.0.1:5000/films')
+    print(r.json())
+    return jsonify(r.json()), 200
